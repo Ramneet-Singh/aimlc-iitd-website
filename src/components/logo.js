81857181fbaecc0c5b2aae592fb2baa3efe8ed13
Ramneet-Singh <ramneet2001@gmail.com>
2020-12-11 05:06:05 +0530

    Changed height and width of image to fixed

--- src/components/logo.js
+++ src/components/logo.js
@@ -4,7 +4,7 @@ import logo from "../../static/assets/club_logo.jpeg"
 
 const Logo = () => (
   <div className="site-logo">
-    <Link to="/"><img src={logo} alt="AI/ML Club IIT Delhi" /></Link>
+    <Link to="/"><img src={logo} alt="AI/ML Club IIT Delhi" style={{height:34, width:187}} /></Link>
   </div>
 )
 
