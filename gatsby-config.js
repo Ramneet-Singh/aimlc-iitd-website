105ffc727cde544ff70d3489d085af1ef7a68ecd
Ramneet-Singh <ramneet2001@gmail.com>
2020-12-12 14:31:34 +0530

    Remove analytics.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,31 +74,16 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-LC9C0Y0H95", // Google Analytics / GA
+          settings.ga, // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
         },
       },
     } ,
-
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: settings.ga,
-      },
-    },
     `gatsby-plugin-advanced-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
