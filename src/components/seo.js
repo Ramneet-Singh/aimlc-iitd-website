192070392f485cfd3acec679ff39a755b57e69d5
Ramneet-Singh <ramneet2001@gmail.com>
2020-12-12 14:07:14 +0530

    Testing Ga-4

--- src/components/seo.js
+++ src/components/seo.js
@@ -27,11 +27,19 @@ const SEO = ({ title, description, image, article }) => {
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <html lang="en-US"/>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LC9C0Y0H95"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LC9C0Y0H95');
+      </script>
       <link rel="alternate" href={seo.url} hreflang="en-us" />
       <link rel="alternate" href={seo.url} hreflang="en" />
       <link rel="alternate" href={seo.url} hreflang="x-default" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet" />      
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
 
