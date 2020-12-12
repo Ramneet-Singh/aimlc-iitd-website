8c9d0d370956ce589131d69526de43d29569d08e
Ramneet-Singh <ramneet2001@gmail.com>
2020-12-12 12:35:44 +0530

    Test for Logo Size and Header Color

--- src/components/logo.js
+++ src/components/logo.js
@@ -4,7 +4,7 @@ import logo from "../../static/assets/aimlc_logo.png"
 
 const Logo = () => (
   <div className="site-logo">
-    <Link to="/"><img src={logo} alt="AI/ML Club IIT Delhi" style={{height:34, width:200}} /></Link>
+    <Link to="/"><img src={logo} alt="AI/ML Club IIT Delhi" style={{height:46, width:270.588235294}} /></Link>
   </div>
 )
 
