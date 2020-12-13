583387ff9314014114280ee9249bf2c3f550450a
Ramneet-Singh <ramneet2001@gmail.com>
2020-12-13 05:48:14 +0530

    Fixed global graphql tag

--- src/components/profile-cards.js
+++ src/components/profile-cards.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link, StaticQuery} from "gatsby"
+import {graphql, StaticQuery} from "gatsby"
 import Img from "gatsby-image"
 
 const ProfileCard = ({ data }) => (
